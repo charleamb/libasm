3eac986d462bdb47b7827366180dea7c3220eaec
Charles-ambroise GILBERT <chgilber@e1r5p20.42.fr>
2020-07-07 19:00:27 +0200

    proto

--- ft_write.s
+++ ft_write.s
@@ -1,26 +1,13 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    ft_strlen.s                                        :+:      :+:    :+:    #
+#    ft_write.s                                         :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: chgilber <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/07/07 18:07:12 by chgilber          #+#    #+#              #
-#    Updated: 2020/07/07 18:18:11 by chgilber         ###   ########.fr        #
+#    Created: 2020/07/07 18:46:15 by chgilber          #+#    #+#              #
+#    Updated: 2020/07/07 18:46:21 by chgilber         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 
-global _ft_strlen
-
-_ft_strlen:		mov			rax,0	; cleqna
-				mov			rcx,0 ;cleqn
-loop:
-				mov			cl,[rdi + rax] ; rdi+rax in cleqna
-				cmp			rcx,0 ; pas finis
-				jne			newloop
-				jmp			end
-newloop:		
-				inc			rax
-				jmp			loop
-end:				ret
