335f97da36d0baf685306d6369e8b8d26b31d6be
Charles-ambroise GILBERT <chgilber@e1r10p20.42.fr>
2020-07-12 21:03:02 +0200

    strcpy

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -6,25 +6,24 @@
 #    By: chgilber <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/07/07 18:45:35 by chgilber          #+#    #+#              #
-#    Updated: 2020/07/11 21:11:37 by chgilber         ###   ########.fr        #
+#    Updated: 2020/07/12 21:02:46 by chgilber         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 global _ft_strcpy
 
-_ft_strcpy:		mov		rax,5
+_ft_strcpy:		mov		rax,0
 
 loop:
-				mov		sil,[rsi]
-				mov		dil,sil
-				mov		al,[rsi]
-				cmp		al,0
+				mov		dl,[rsi + rax]
+				cmp		dl,0
 				je		end
-				inc		rdi
-				inc		rsi
+				mov		[rdi + rax],dl
 				inc		rax
 				jmp		loop
 end:
+				mov		[rdi + rax],dl
+				mov		rax,rdi
 				ret
 
 
