6dc006c3beb49ade481c3d8b606f5b2840138264
Charles-ambroise GILBERT <chgilber@e2r5p4.42.fr>
2020-07-29 15:59:04 +0200

    dup

--- libasmlinux/ft_strdup.s
+++ libasmlinux/ft_strdup.s
@@ -6,20 +6,20 @@
 #    By: chgilber <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/07/07 18:46:57 by chgilber          #+#    #+#              #
-#    Updated: 2020/07/28 18:27:53 by chgilber         ###   ########.fr        #
+#    Updated: 2020/07/29 15:58:47 by chgilber         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 		global	ft_strdup
 		extern	ft_strlen
 		extern	ft_strcpy
-		extern	_malloc
+		extern	malloc
 
 ft_strdup:		call	ft_strlen ; rax = len
 				inc		rax			; len ++ '\0'
 				push	rdi          ; stock de str
 				mov		rdi,rax		;1er arg = len a malloc
-				call	_malloc
+				call	malloc
 				cmp		rax,0
 				je		error
 				pop		rsi			;2eme arg = str stocker
